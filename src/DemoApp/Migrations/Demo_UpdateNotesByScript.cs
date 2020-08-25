ebc2cb544e96ed27649c46a1dafe76574a951e7a
congzw <2981810300@qq.com>
2020-08-25 17:57:31 +0800

    add more demo

---
+++ src/DemoApp/Migrations/Demo_UpdateNotesByScript.cs
@@ -0,0 +1,32 @@
+﻿//using FluentMigrator;
+
+//namespace DemoApp.Migrations
+//{
+//    public class Demo_UpdateNotesByScript
+//    {
+
+//        /// <summary>
+//        /// Update notes.
+//        /// </summary>
+//        /// <remarks>
+//        /// This is only run when the migration 20090906205440 was already applied to the database.
+//        /// </remarks>
+//        [Migration(20190416112000)]
+//        public class UpdateNotesByScript : Migration
+//        {
+//            /// <inheritdoc />
+//            public override void Up()
+//            {
+//                Execute.Sql(@"/* this is a test script */
+//update Notes set body=body || ' (modified)';
+//");
+//            }
+
+//            /// <inheritdoc />
+//            public override void Down()
+//            {
+//                // Nothing to do here
+//            }
+//        }
+//    }
+//}
